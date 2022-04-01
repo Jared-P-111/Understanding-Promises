7571c5fb125d80eac88713d408b963f364aaafb9
Jared_P <jaredpisell@gmail.com>
2022-03-31 23:35:24 -0700

    Added Promises summary and Promises Chaining to lessons

--- Promises/indexPromises.js
+++ Promises/indexPromises.js
@@ -1,19 +1,4 @@
 
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data)
-        } else if(request.readyState === 4){
-            callback('Could not fetch data', undefined);
-        }
-    })
-    request.open('GET', resource);
-    request.send();
-}
-
 // Promise Example
 //First thing with promises is return a new Promise() remember that a promise has two outcomes.
 //1: it is an error or it resolves to doing something. 
@@ -21,9 +6,41 @@ const getSomething = () => {
     //Promises have the keywords "resolve" and "reject" built into the API
     return new Promise((resolve, reject) => {
         //fetch something
-        resolve('some data');
-        reject('some error');
+        resolve('some data');//<- Passed through to the .then 
+        reject('some error');//<-- also if there passed through to the .then
     });
 }
 
 
+//By calling getSomething it instantiates the new Promise.
+//When we get a new promise back from a function like below. We can use the .then method.
+//By passing a function into the .then its saying okay when the promise is returned I will fire this 
+//function. "Callback" remember the data is passed through. Only one of the two can fire.if its an error 
+//the catch will get it if its data .then will pass it. 
+getSomething().then((data) => {
+    console.log(data);
+}).catch(error => {
+    console.log(error);
+});
+
+//This is a fully functional promise with api call this is the working example of above. 
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) =>{
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            if(request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if(request.readyState === 4){
+                reject('Error getting resource');
+            }
+        })
+        request.open('GET', resource);
+        request.send();
+    })
+}
+getTodos('./todos/luigis.json').then(data => {
+    console.log('promise resolved', data);
+}).catch(error => {
+    console.log('promise rejected', error);
+})
