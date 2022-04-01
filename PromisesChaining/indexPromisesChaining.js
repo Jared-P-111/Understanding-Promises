4c2b0e07141ec8d00200d1d464ddb1d535c08dd9
Jared_P <jaredpisell@gmail.com>
2022-04-01 10:27:13 -0700

    Added Promises Chaining lesson

--- PromisesChaining/indexPromisesChaining.js
+++ PromisesChaining/indexPromisesChaining.js
@@ -1,3 +1,7 @@
+//The get todos functiont that creates a promise each time it is called.
+//the promise then uses an XMLHttpRequest to "GET" the data from the API.
+//From that API call we can determine if its an error or a resolved call. 
+
 const getTodos = (resource) => {
     return new Promise((resolve, reject) =>{
         const request = new XMLHttpRequest();
@@ -13,8 +17,19 @@ const getTodos = (resource) => {
         request.send();
     })
 }
-getTodos('./todos/luigis.json').then(data => {
-    console.log('promise resolved', data);
+
+//This is promise chaining the return gives a new promise which lets us call the next request
+//The thing to note is that the catch function at the end will catch any error with any chained
+//promise. 
+
+getTodos('./todos/luigi.json').then(data => {
+    console.log('promise 1 resolved', data);
+    return getTodos('./todos/mario.json');
+}).then(data => {
+    console.log('promise 2 resolved:', data)
+    return getTodos('./todos/yoshi.json');
+}).then(data => {
+    console.log('promise 3 resolved:', data)
 }).catch(error => {
     console.log('promise rejected', error);
 })
